31f88e6b8a7e831190197cd659b42f80bf59d046
GonzaloVegaRamos <brevavega@gmail.com>
2025-02-19 19:38:01 +0100

    mas

--- interfaces/src/main/java/com/example/MainController.java
+++ interfaces/src/main/java/com/example/MainController.java
@@ -1,4 +1,3 @@
-
 package com.example;
 
 import java.io.FileOutputStream;
@@ -11,7 +10,11 @@ import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 
 public class MainController {
     @FXML
@@ -76,6 +79,24 @@ public class MainController {
         }
     }
 
+    // Método que abre la ventana de CRUD
+    @FXML
+    private void abrirCRUD() {
+        try {
+            // Cargar el archivo FXML de la vista CRUD
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/CRUDView.fxml"));
+            Parent root = loader.load();
+            
+            // Crear una nueva ventana para mostrar la vista CRUD
+            Stage stage = new Stage();
+            stage.setTitle("Gestión de CRUD");
+            stage.setScene(new Scene(root, 400, 300));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private void mostrarDatos(String sql) {
         resultadoArea.clear(); 
 
@@ -83,51 +104,48 @@ public class MainController {
             ResultSet resultSet = dbConnection.executeQuery(sql);
 
             StringBuilder resultado = new StringBuilder();
-            
 
-if (sql.contains("Productos")) {
-    resultado.append(String.format("%-5s %-20s %-10s %-10s%n", "ID", "Nombre", "Precio", "Stock"));
-    resultado.append("---------------------------------------------------\n");
-    
-    while (resultSet.next()) { // Iteramos sobre todos los resultados
-        int id = resultSet.getInt("id_producto");
-        String nombre = resultSet.getString("nombre");
-        double precio = resultSet.getDouble("precio");
-        int stock = resultSet.getInt("stock");
+            if (sql.contains("Productos")) {
+                resultado.append(String.format("%-5s %-20s %-10s %-10s%n", "ID", "Nombre", "Precio", "Stock"));
+                resultado.append("---------------------------------------------------\n");
+                
+                while (resultSet.next()) { // Iteramos sobre todos los resultados
+                    int id = resultSet.getInt("id_producto");
+                    String nombre = resultSet.getString("nombre");
+                    double precio = resultSet.getDouble("precio");
+                    int stock = resultSet.getInt("stock");
 
-        resultado.append(String.format("%-5d %-20s %-10.2f %-10d%n", id, nombre, precio, stock));
-    }
-} else if (sql.contains("Empleados")) {
-    resultado.append(String.format("%-5s %-20s %-15s %-15s%n", "ID", "Nombre", "Cargo", "Fecha Contratación"));
-    resultado.append("---------------------------------------------------------------\n");
+                    resultado.append(String.format("%-5d %-20s %-10.2f %-10d%n", id, nombre, precio, stock));
+                }
+            } else if (sql.contains("Empleados")) {
+                resultado.append(String.format("%-5s %-20s %-15s %-15s%n", "ID", "Nombre", "Cargo", "Fecha Contratación"));
+                resultado.append("---------------------------------------------------------------\n");
 
-    while (resultSet.next()) {
-        int id = resultSet.getInt("id_empleado");
-        String nombre = resultSet.getString("nombre");
-        String cargo = resultSet.getString("cargo");
-        String fechaContratacion = resultSet.getString("fecha_contratacion");
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id_empleado");
+                    String nombre = resultSet.getString("nombre");
+                    String cargo = resultSet.getString("cargo");
+                    String fechaContratacion = resultSet.getString("fecha_contratacion");
 
-        resultado.append(String.format("%-5d %-20s %-15s %-15s%n", id, nombre, cargo, fechaContratacion));
-    }
-} 
-else if (sql.contains("Ventas")) {
-    resultado.append(String.format("%-5s %-12s %-12s %-10s %-15s %-10s%n", 
-        "ID", "ID Emp", "ID Prod", "Cantidad", "Fecha Venta", "Total"));
-    resultado.append("------------------------------------------------------------------\n");
+                    resultado.append(String.format("%-5d %-20s %-15s %-15s%n", id, nombre, cargo, fechaContratacion));
+                }
+            } else if (sql.contains("Ventas")) {
+                resultado.append(String.format("%-5s %-10s %-10s %-10s %-15s %-10s%n", 
+                    "ID", "ID Emp", "ID Prod", "Cantidad", "Fecha Venta", "Total"));
+                resultado.append("------------------------------------------------------------------\n");
 
-    while (resultSet.next()) {
-        int id = resultSet.getInt("id_venta");
-        int idEmpleado = resultSet.getInt("id_empleado");
-        int idProducto = resultSet.getInt("id_producto");
-        int cantidad = resultSet.getInt("cantidad");
-        String fechaVenta = resultSet.getString("fecha_venta");
-        double totalVenta = resultSet.getDouble("total_venta");
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id_venta");
+                    int idEmpleado = resultSet.getInt("id_empleado");
+                    int idProducto = resultSet.getInt("id_producto");
+                    int cantidad = resultSet.getInt("cantidad");
+                    String fechaVenta = resultSet.getString("fecha_venta");
+                    double totalVenta = resultSet.getDouble("total_venta");
 
-        resultado.append(String.format("%-8d %-15d %-15d %-13d %-15s %10.2f%n", 
-            id, idEmpleado, idProducto, cantidad, fechaVenta, totalVenta));
-    }
-}
-
+                    resultado.append(String.format("%-8d %-15d %-15d %-11d %-15s %10.2f%n", 
+                        id, idEmpleado, idProducto, cantidad, fechaVenta, totalVenta));
+                }
+            }
 
             resultadoArea.setText(resultado.toString()); 
             dbConnection.closeResultSet(resultSet); 
@@ -136,10 +154,3 @@ else if (sql.contains("Ventas")) {
         }
     }
 }
-
-
-
-
-
-
-
